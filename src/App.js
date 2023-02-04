47e31a88853e0ce1108e62e43fea07fd0743193d
Perestra <danilo.ps.1996@gmail.com>
2023-02-04 11:51:40 -0300

    Inserted the Date component into App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import './App.css';
+import React from "react"
+import Date from "./Components/Date/Date";
 
 function App() {
   return (
     <div className="App">
+      <main className="main_content">
+        <Date />
+      </main>
     </div>
   );
 }
