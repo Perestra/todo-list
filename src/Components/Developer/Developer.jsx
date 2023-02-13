1429dd322259728c241edc256a50553560c2abff
Perestra <danilo.ps.1996@gmail.com>
2023-02-12 21:16:29 -0300

    Created a footer component

---
+++ src/Components/Developer/Developer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../../Styles/Developer.css'
+
+const Developer = () => {
+  return (
+    <footer className='footer'>
+        <div className='developer'>
+            <p>Developed by <strong>Danilo Perestrelo</strong></p>
+        </div>
+    </footer>
+  )
+}
+
+export default Developer
