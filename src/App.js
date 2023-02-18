5f49c15fdee445b1ad2af7189685333e2391896b
Perestra <danilo.ps.1996@gmail.com>
2023-02-17 21:04:22 -0300

    Implemented the useLocalStorage hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react"
+import React from "react";
+import useLocalStorage from "use-local-storage";
 import Date from "./Components/Date/Date";
 import EnterTask from "./Components/EnterTask/EnterTask";
 import List from "./Components/List/List";
@@ -7,22 +8,23 @@ import Developer from "./Components/Developer/Developer";
 import { currentWeekDay, currentDate, currentMonth } from './Utils/getDate/get-date'
 
 const App = () => {
-
-  const [tasks, setTasks] = useState([])
+ 
+  const [tasks, setTasks] = useLocalStorage('Task', [])
 
   const addItem = task => {
     setTasks([...tasks, task])
   }
+
   const deleteItem = id => {
     let filteredTasks = tasks.filter(task => task.id !== id)
     setTasks(filteredTasks)
   }
-
+  
   return (
     <div className="App">
       <main className="main_content">
         <Date weekDay={ currentWeekDay } date={ currentDate } month={ currentMonth } />
-        <EnterTask submitItem={ addItem } tasksLength={ tasks.length } />
+        <EnterTask submitItem={ addItem } tasksLength={ tasks?.length || 0 } />
         <List list={ tasks } deleteItemList={ deleteItem }/>
         <Developer />
       </main>
