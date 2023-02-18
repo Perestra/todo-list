bf74ad0dd05251046dfc6ec651706218142b42ef
Perestra <danilo.ps.1996@gmail.com>
2023-02-17 21:04:52 -0300

    Added a verification in the list.map

--- src/Components/List/List.jsx
+++ src/Components/List/List.jsx
@@ -7,7 +7,7 @@ const List = ({ list, deleteItemList}) => {
     <section className='list_section'>
       <div className='list_container'>
         <ul className='list'>
-          {list.map((item) => <ItemList key= { item.id } taskName= { item.taskName } deleteItemList={ () => deleteItemList(item.id) } />)}
+          {list?.map((item) => <ItemList key= { item.id } taskName= { item.taskName } deleteItemList={ () => deleteItemList(item.id) } />)}
         </ul>
       </div>
     </section>
